9b59676ab733136274d8ef5774d16fdf08a9b999
MrDinhHoa <68648708+MrDinhHoa@users.noreply.github.com>
2025-06-26 19:43:23 +0700

    Add files via upload

---
+++ DHHTool_RS_Beam_2DCrossSection/MVVM/View/vMainRS2DCrossSection.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DHHTool_RS_Beam_2DCrossSection.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for vMainRS2DCrossSection.xaml
+    /// </summary>
+    public partial class vMainRS2DCrossSection : Window
+    {
+        public vMainRS2DCrossSection()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DHHTool_RS_Beam_2DCrossSection/RSBeam_2DCrossSection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.Attributes;
+using System.Windows;
+using DHHTool_RS_Beam_2DCrossSection.MVVM.View;
+
+namespace DHHTool_RS_Beam_2DCrossSection
+{
+    [Transaction(TransactionMode.Manual)]
+    public class RSBeam_2DCrossSection : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            //Autodesk.Revit.ApplicationServices.Application app = uiapp.Application;
+            Document document = uidoc.Document;
+            try
+            {
+                using (TransactionGroup transGroup = new TransactionGroup(document))
+                {
+                    transGroup.Start("Thống kê thép dầm");
+                    vMainRS2DCrossSection win = new vMainRS2DCrossSection();
+                    bool? dialog = win.ShowDialog();
+                    if (dialog != false)
+                        return Result.Succeeded;
+                    transGroup.Commit();
+                }
+            }
+            catch { }
+            return Result.Succeeded;
+
+        }
+    }
+}
